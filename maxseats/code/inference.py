4c2aea6bc59a1226e40de289c97480fee00786a8
kms <kms-git0205@github.com>
2023-12-15 09:00:24 +0900

    [score 0.9508 과적합 예상] model_name kykim_electra-ko-base batch_size 8 epoch 100

--- maxseats/code/inference.py
+++ maxseats/code/inference.py
@@ -174,8 +174,8 @@ if __name__ == '__main__':
     ######################################################################
     #디폴트 : klue/roberta-small, 16, 1, True, 1e-5
     one_model_name = 'kykim/electra-kor-base'
-    two_batch_size = 32
-    three_max_epoch = 20
+    two_batch_size = 8
+    three_max_epoch = 100
     four_shuffle = True
     five_learning_rate = 1e-5
     ######################################################################
