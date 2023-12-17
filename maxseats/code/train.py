183dd22109e107a45be620b3e6616d27a387c50e
kms <kms-git0205@github.com>
2023-12-18 08:56:47 +0900

    kykim/electra-kor-base 16 20 1e-5 stopword_space_Addlabel5.csv

--- maxseats/code/train.py
+++ maxseats/code/train.py
@@ -36,9 +36,9 @@ snunlp/KR-ELECTRA-discriminator
 
 one_model_name = 'kykim/electra-kor-base'
 two_batch_size = 16
-three_max_epoch = 30
+three_max_epoch = 20
 four_shuffle = True
-five_learning_rate = 0.00001108
+five_learning_rate = 1e-5
 
 #six_train_path = '/data/ephemeral/home/code/stopword_space_Addlabel5.csv'   #불용어처리+띄어쓰기 -> Label5.0 증강
 six_train_path = '/data/ephemeral/home/code/stopword_space_Addlabel5.csv'
@@ -274,7 +274,7 @@ if __name__ == '__main__':
     trainer.test(model=model, datamodule=dataloader)
 
     # 학습이 완료된 모델을 저장합니다.
-    torch.save(model, 'model2.pt')
+    torch.save(model, 'model.pt')
 
     # [W&B] 학습이 완료되면 마지막에 W&B run을 종료합니다.
     wandb.finish()
